df2988e1443fdb7cf793759a341fdf571a53d0e7
Asif Tuhin <tuhin123000@gmail.com>
2022-11-13 18:51:06 +0600

    Full

---
+++ UtillApp/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const {eddsa} = require('elliptic');
+const cors = require('cors');
+
+
+const ec = new eddsa('ed25519');
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/',async (req,res) => {
+    const { secret , obj } = req.body;
+    const key = ec.keyFromSecret(secret);
+    const public_key = key.getPublic();
+    const hex_public_key = new Buffer.from(public_key).toString('hex');
+    const msg = JSON.stringify(obj);
+    const sign = key.sign(msg).toHex();
+    res.json({
+        msg : 'success',
+        hex_public_key,
+        sign
+    });
+    console.log('response sent')
+});
+
+app.listen(3005,()=>{
+    console.log('listening on port 3005');
+});
